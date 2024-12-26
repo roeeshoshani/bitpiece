b6fca02c008716a9a2136570ef855e195c283026
Roee Shoshani <roysho10@gmail.com>
2024-12-26 03:51:12 +0200

    use enums in tests

--- tests/basic.rs
+++ tests/basic.rs
@@ -1,10 +1,19 @@
 use bitpiece::*;
 
-#[bitpiece(5)]
+#[bitpiece(2)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum BitPieceEnum {
+    Variant0 = 0,
+    Variant1 = 1,
+    Variant2 = 2,
+    Variant3 = 3,
+}
+
+#[bitpiece(3)]
 #[derive(Debug, Clone, Copy)]
 struct BitPieceA {
     x: bool,
-    y: B4,
+    y: BitPieceEnum,
 }
 
 #[bitpiece(35)]
@@ -14,7 +23,7 @@ struct BitPieceB {
     y: B3,
 }
 
-#[bitpiece(40)]
+#[bitpiece(38)]
 #[derive(Debug, Clone, Copy)]
 struct BitPieceComplex {
     a: BitPieceA,
@@ -23,36 +32,36 @@ struct BitPieceComplex {
 
 #[test]
 fn bit_extraction() {
-    assert_eq!(BitPieceA::from_bits(0b00001).x(), true);
-    assert_eq!(BitPieceA::from_bits(0b11111).x(), true);
-    assert_eq!(BitPieceA::from_bits(0b00000).x(), false);
-    assert_eq!(BitPieceA::from_bits(0b11110).x(), false);
+    assert_eq!(BitPieceA::from_bits(0b001).x(), true);
+    assert_eq!(BitPieceA::from_bits(0b111).x(), true);
+    assert_eq!(BitPieceA::from_bits(0b000).x(), false);
+    assert_eq!(BitPieceA::from_bits(0b110).x(), false);
 
-    assert_eq!(BitPieceA::from_bits(0b10110).y().0, 0b1011);
-    assert_eq!(BitPieceA::from_bits(0b11101).y().0, 0b1110);
+    assert_eq!(BitPieceA::from_bits(0b110).y(), BitPieceEnum::Variant3);
+    assert_eq!(BitPieceA::from_bits(0b101).y(), BitPieceEnum::Variant2);
 }
 
 #[test]
 fn bit_modification() {
     let mut value = BitPieceA::zeroed();
     assert_eq!(value.x(), false);
-    assert_eq!(value.y().0, 0);
+    assert_eq!(value.y(), BitPieceEnum::Variant0);
     assert_eq!(value.storage, 0);
 
     value.set_x(true);
     assert_eq!(value.x(), true);
-    assert_eq!(value.y().0, 0);
-    assert_eq!(value.storage, 0b00001);
+    assert_eq!(value.y(), BitPieceEnum::Variant0);
+    assert_eq!(value.storage, 0b001);
 
-    value.set_y(B4(0b1011));
+    value.set_y(BitPieceEnum::Variant3);
     assert_eq!(value.x(), true);
-    assert_eq!(value.y().0, 0b1011);
-    assert_eq!(value.storage, 0b10111);
+    assert_eq!(value.y(), BitPieceEnum::Variant3);
+    assert_eq!(value.storage, 0b111);
 
     value.set_x(false);
     assert_eq!(value.x(), false);
-    assert_eq!(value.y().0, 0b1011);
-    assert_eq!(value.storage, 0b10110);
+    assert_eq!(value.y(), BitPieceEnum::Variant3);
+    assert_eq!(value.storage, 0b110);
 }
 
 #[test]
